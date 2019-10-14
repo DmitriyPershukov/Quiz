3492eba896922d66428a45501248e3ea465beefc
DmitriyPershukov <dmitriypershukov@gmail.com>
2019-10-14 14:16:14 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        //Console.startConsole("Supply/questions.txt");
+        //Console.startConsole();
         Telegram.init();
     }
 }
