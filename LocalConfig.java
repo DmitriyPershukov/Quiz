a3c197f8172d7d2a5f91ab23fe364baee1a700d2
DmitriyPershukov <dmitriypershukov@gmail.com>
2019-10-15 15:11:36 +0500

    Delete LocalConfig.java

--- LocalConfig.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class LocalConfig
-{
-    public static String botToken = "653958636:AAEPjcFM0AY0fVCloKRsinujpezFNzq3a1M";
-    public static String botName = "oopDPxBdEbot";
-}
